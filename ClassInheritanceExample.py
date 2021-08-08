bda8ee31e4a61bd3d45f658f77fa690142a64231
eisbilen <erdemisbilen@gmail>
2021-08-08 14:10:40 +0100

    commit on master

--- ClassInheritanceExample.py
+++ ClassInheritanceExample.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import random
+import json
 import requests
 import os
+from datetime import datetime
 
 from bs4 import BeautifulSoup
+
+import re
+
 
 PATH = ""
 FILE_NAME_01 = "data_by_artists.csv"
@@ -96,28 +101,35 @@ if __name__ == '__main__':
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
 
-    questionlist = []
+    articleLinks = set()
+    dataAll = []
 
-    def getQuestions(tag, page):
-        url = f'https://stackoverflow.com/questions/tagged/{tag}?tab=Active&page={page}&pagesize=50'
+    def getArticleLinks(tag, page):
+        url = f'https://www.trtworld.com/{tag}?page={page}'
         r = requests.get(url, headers=headers)
         soup = BeautifulSoup(r.text, 'html.parser')
-        questions = soup.find_all('div', {'class': 'question-summary'})
-        for item in questions:
-            question = {
-            'tag': tag,    
-            'title': item.find('a', {'class': 'question-hyperlink'}).text,
-            'link': 'https://stackoverflow.com' + item.find('a', {'class': 'question-hyperlink'})['href'],
-            'votes': int(item.find('span', {'class': 'vote-count-post'}).text),
-            'date': item.find('span', {'class': 'relativetime'})['title'],
-            }
-            questionlist.append(question)
+        
+        articleLinks = {'https://www.trtworld.com' + a['href'] for a in soup.find_all('a', {'class': 'gtm-topic-latest-article'})}
+
+        for articleLink in articleLinks:
+            r = requests.get(articleLink, headers=headers)
+            item = BeautifulSoup(r.text, 'html.parser')
+            content = item.find("div", {"class":"contentBox bg-w noMedia"})
+
+            if content:
+                content = content.findAll('p')
+                data = {'tag': tag,
+                        'datetime': datetime.now().strftime("%d/%m/%Y, %H:%M:%S"),  
+                        'title': item.find('h1', {'class': 'article-title'}).text,
+                        'website': 'trtworld',
+                        'link': url,
+                        'article': [p.text for p in content]}
+                print(data)
+                dataAll.append(data)
+
+        with open('/data/articles_'+datetime.now().strftime("%Y-%m-%d-%H-%M-%S")+'.json', 'w') as outfile:
+            json.dump(dataAll, outfile)
+
         return
 
-    for x in range(1,3):
-        getQuestions('python', x)
-        getQuestions('flask', x)
-
-    df = pd.DataFrame(questionlist)
-    df.to_excel('/data/stackquestions.xlsx', index=False)
-    print('Fin.')
+    getArticleLinks('Sport', 1)
